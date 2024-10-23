47c89a0d9bc838332625d4930d17bb64fac89cd4
Rene <rene.hernandez@ejje.com>
2024-10-23 16:32:55 -0600

    Initial Commit

---
+++ doctornet/doctornetApp.swift
@@ -0,0 +1,17 @@
+//
+//  doctornetApp.swift
+//  doctornet
+//
+//  Created by Grupo EJJE on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct doctornetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ doctornetTests/doctornetTests.swift
@@ -0,0 +1,17 @@
+//
+//  doctornetTests.swift
+//  doctornetTests
+//
+//  Created by Grupo EJJE on 23/10/24.
+//
+
+import Testing
+@testable import doctornet
+
+struct doctornetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
